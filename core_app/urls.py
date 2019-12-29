45401745cbc575e7bee2031266e2baa2a53277fc
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2019-12-29 03:44:55 -0700

    Added logout functionality; modified core_app/urls.py and views.py. Fixed template issues while rendering blocks on home, login and base html files.

--- core_app/urls.py
+++ core_app/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("",Home.as_view(), name="home"),
     path("login/", auth_views.LoginView.as_view(template_name="core_app/login.html"),
-        name="login")
-
+        name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="core_app/login.html"),
+        name="logout")
 ]
--- core_app/views.py
+++ core_app/views.py
@@ -6,4 +6,4 @@ from django.views import generic
 # Create your views here.
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name= "core_app/home.html"
-    login_url= "login/"
+    login_url= "core_app:login"
