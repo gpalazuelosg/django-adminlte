7cae3aa49c7889867b53a919d528aa9ab49860b0
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2019-12-29 02:54:17 -0700

    Added login page, with view and url setup, as well added login.html. Modified base/base.html to add block for login. Relocated blocks to fix rendering problems in core_app/home.html. Updated notes.txt

--- core_app/urls.py
+++ core_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 
 from .views import Home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',Home.as_view(), name="home"),
+    path("",Home.as_view(), name="home"),
+    path("login/", auth_views.LoginView.as_view(template_name="core_app/login.html"),
+        name="login")
+
 ]
