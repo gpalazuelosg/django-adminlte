1f808a6190575a63026f28446ae530102b466257
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2020-01-03 23:12:43 -0700

    Added ModelClass on core_app/models.py

--- core_app/models.py
+++ core_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class ModelClass(models.Model):
+    """Base class for my solution for common fields."""
+
+    active = models.BooleanField(default=True)
+    createdon = models.DateTimeField(auto_now_add=True)
+    createdby = models.ForeignKey(User, on_delete=models.CASCADE)
+    lastupdatedon = models.DateTimeField(auto_now=True)
+    lastupdatedby = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
