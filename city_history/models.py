b12e8d798a156bd3b5d12d3fede2df69648d6a7f
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2020-01-03 23:23:06 -0700

    Added Country class on city_history/models.py

--- city_history/models.py
+++ city_history/models.py
@@ -1,3 +1,24 @@
+# Django
 from django.db import models
 
+# solution
+from core_app.models import ModelClass
+
 # Create your models here.
+
+class Country(ModelClass):
+    name = models.CharField(
+        max_length=100,
+        help_text="Country name",
+        unique=True
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def save(self):
+        self.name = self.name.upper()
+        super(Country,self).save()
+
+    class Meta:
+        verbose_name_plural = "Countries"
