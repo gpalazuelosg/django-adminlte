092f4c66a3ba5da8109d91ea7842828084d1926b
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2020-01-23 22:40:26 -0700

    Added city_history/forms.py. Added url instruction on country_list to target view country_new. Added entry in city_history/urls.py for country_new. Added CountryNew class in city_history/views.py

--- city_history/urls.py
+++ city_history/urls.py
@@ -1,8 +1,9 @@
 # Django
 from django.urls import path
 
-from .views import CountryView
+from .views import CountryView, CountryNew
 
 urlpatterns = [
     path('countries/', CountryView.as_view(), name="country_list"),
+    path('countries/new', CountryNew.as_view(), name="country_new"),
 ]
--- city_history/views.py
+++ city_history/views.py
@@ -1,11 +1,14 @@
 # Django
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # solution
 from .models import Country
+from .forms import CountryForm
+
 
 # Create your views here.
 class CountryView(LoginRequiredMixin, generic.ListView):
@@ -13,3 +16,15 @@ class CountryView(LoginRequiredMixin, generic.ListView):
     template_name = "city_history/country_list.html"
     context_object_name = "obj"
     login_url = "core_app:login"
+
+class CountryNew(LoginRequiredMixin, generic.CreateView):
+    model = Country
+    template_name = "city_history/country_form.html"
+    context_object_name = "obj"
+    form_class = CountryForm
+    success_url = reverse_lazy("city_history:country_list")
+    login_url = "core_app:login"
+
+    def form_valid(self, form):
+        form.instance.createdby = self.request.user
+        return super.form_valid(form)
