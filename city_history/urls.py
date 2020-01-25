a9658f576d4872ce712ee349e484865b62aafc1c
Gerardo Palazuelos @ MacBookTacho <gerardo.palazuelos@gmail.com>
2020-01-24 22:47:38 -0700

    Added new pattern for country_edit in urls.py. Added some filters for Active and dates fields in country_list.html. Fixed a typo in country_form.html

--- city_history/urls.py
+++ city_history/urls.py
@@ -1,9 +1,10 @@
 # Django
 from django.urls import path
 
-from .views import CountryView, CountryNew
+from .views import CountryView, CountryNew, CountryEdit
 
 urlpatterns = [
     path('countries/', CountryView.as_view(), name="country_list"),
     path('countries/new', CountryNew.as_view(), name="country_new"),
+    path('countries/edit/<int:pk>', CountryEdit.as_view(), name="country_edit"),
 ]
