65e56382a15c2d3dc60a7aff11534c2fabe0effc
Gerardo Palazuelos <gerardo.palazuelos@gmail.com>
2020-02-03 22:55:06 -0700

    Added class States in models.py

--- city_history/models.py
+++ city_history/models.py
@@ -24,3 +24,27 @@ class Country(ModelClass):
 
     class Meta:
         verbose_name_plural = "Countries"
+
+
+class State(ModelClass):
+    """ State class for this solution which is linked to one Country entity. Inherits from ModelClass. """
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length= 100,
+        help_text= "State name",
+        blank= False,
+        null= False
+    )
+
+    def __str__(self, *args, **kwargs):
+        return f"{self.country.name}:{self.name}"
+    
+    def save(self):
+        self.name = self.name.upper()
+        super(State.self).save()
+
+    class Meta:
+        verbose_name_plural = "States"
+
+        # es un unique compuesto
+        unique_together = ("country", "name")
