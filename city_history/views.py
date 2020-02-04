3d93c32fa3ddac5011caa63f876ce75900186552
Gerardo Palazuelos <gerardo.palazuelos@gmail.com>
2020-02-03 23:30:18 -0700

    Added class StateNew in views.py

--- city_history/views.py
+++ city_history/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # solution
 from .models import Country, State
-from .forms import CountryForm
+from .forms import CountryForm, StateForm
 
 
 # Create your views here.
@@ -47,8 +47,22 @@ class CountryDelete(LoginRequiredMixin, generic.DeleteView):
     context_object_name = "obj"
     success_url = reverse_lazy("city_history:country_list")
 
+
 class StateView(LoginRequiredMixin, generic.ListView):
     model = State
     template_name = "city_history/state_list.html"
     context_object_name = "obj"
     login_url = "core_app:login"
+
+
+class StateNew(LoginRequiredMixin, generic.CreateView):
+    model = State
+    template_name = "city_history/state_form.html"
+    context_object_name = "obj"
+    form_class = CountryForm
+    success_url = reverse_lazy("city_history:state_list")
+    login_url = "core_app:login"
+
+    def form_valid(self, form):
+        form.instance.createdby = self.request.user
+        return super().form_valid(form)
