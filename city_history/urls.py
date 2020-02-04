66f36b18d4346b23a328a2d3e3d62fb8d2c404c8
Gerardo Palazuelos <gerardo.palazuelos@gmail.com>
2020-02-03 23:15:10 -0700

    Modified base.html to include link to States List. Modified urls.py to include settings for States List. Minor update on title in template state_list.html

--- city_history/urls.py
+++ city_history/urls.py
@@ -1,11 +1,14 @@
 # Django
 from django.urls import path
 
-from .views import CountryView, CountryNew, CountryEdit, CountryDelete
+from .views import CountryView, CountryNew, CountryEdit, CountryDelete, \
+    StateView
 
 urlpatterns = [
     path('countries/', CountryView.as_view(), name="country_list"),
     path('countries/new', CountryNew.as_view(), name="country_new"),
     path('countries/edit/<int:pk>', CountryEdit.as_view(), name="country_edit"),
     path('countries/delete/<int:pk>', CountryDelete.as_view(), name="country_delete"),
+
+    path('states/', StateView.as_view(), name="state_list"),
 ]
